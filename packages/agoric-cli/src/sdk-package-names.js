1366ab03a8d5c384c42f14c0f0e9b3cee8b234a3
rabi-siddique <rabisiddique27@gmail.com>
2025-06-12 07:56:29 +0500

    add a package for doing experiments

--- packages/agoric-cli/src/sdk-package-names.js
+++ packages/agoric-cli/src/sdk-package-names.js
@@ -54,5 +54,6 @@ export default [
   "@agoric/xsnap-lockdown",
   "@agoric/zoe",
   "@agoric/zone",
-  "agoric"
+  "agoric",
+  "agoric-contract-experiments"
 ];
---
+++ packages/experiments/src/builder.js
@@ -0,0 +1,28 @@
+import { makeHelpers } from '@agoric/deploy-script-support';
+import { getManifest, startContract } from './proposal.js';
+
+/**
+ * @import {CoreEvalBuilder} from '@agoric/deploy-script-support/src/externalTypes.js'
+ */
+
+/** @type {CoreEvalBuilder} */
+export const defaultProposalBuilder = async ({ publishRef, install }) => {
+  return harden({
+    sourceSpec: './proposal.js',
+    getManifestCall: [
+      getManifest.name,
+      {
+        installKeys: {
+          counter: publishRef(install('./contract.js')),
+        },
+      },
+    ],
+  });
+};
+
+export default async (homeP, endowments) => {
+  const { writeCoreEval } = await makeHelpers(homeP, endowments);
+  await writeCoreEval(startContract.name, (opts) =>
+    defaultProposalBuilder(opts)
+  );
+};
