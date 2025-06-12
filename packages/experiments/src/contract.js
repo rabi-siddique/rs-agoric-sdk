5d1f25c6dc1c0075507640ba58b948336b043667
rabi-siddique <rabisiddique27@gmail.com>
2025-06-12 07:56:29 +0500

    add a package for doing experiments

---
+++ packages/experiments/src/contract.js
@@ -0,0 +1,51 @@
+import { E } from '@endo/far';
+import { prepareExo } from '@agoric/vat-data';
+import { M } from '@agoric/store';
+
+let counter = 0;
+export const start = async (zcf, { storageNode }, baggage) => {
+  console.log('Inside the contract');
+
+  const publicFacet = prepareExo(
+    baggage,
+    'Counter Public Facet',
+    M.interface('Counter PF', {
+      getCounter: M.call().returns(M.any()),
+      makeIncrementInvitation: M.call().returns(M.any()),
+      makeDecrementInvitation: M.call().returns(M.any()),
+    }),
+    {
+      getCounter() {
+        return counter;
+      },
+      makeIncrementInvitation() {
+        return zcf.makeInvitation(
+          async (seat) => {
+            seat.exit();
+            counter += 1;
+            await E(storageNode).setValue(String(counter));
+            console.log('Counter Value:', counter);
+          },
+          'increment counter',
+          undefined
+        );
+      },
+      makeDecrementInvitation() {
+        return zcf.makeInvitation(
+          async (seat) => {
+            seat.exit();
+            counter -= 1;
+            await E(storageNode).setValue(String(counter));
+            console.log('Counter Value:', counter);
+          },
+          'decrement counter',
+          undefined
+        );
+      },
+    }
+  );
+
+  return {
+    publicFacet,
+  };
+};
