32352aa140b6da449528f477bf4f8b02e43c64fb
rabi-siddique <rabisiddique27@gmail.com>
2025-09-03 22:39:54 +0500

    chore: script for making an nfa

---
+++ packages/experiments/scripts/makeNfa.mjs
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+// @ts-check
+import './lockdown.mjs';
+import {
+  fetchFromVStorage,
+  poll,
+  prepareOffer,
+  processWalletOffer,
+  validateEvmAddress,
+} from './utils.mjs';
+
+const OFFER_FILE = 'offer.json';
+const CONTAINER_PATH = `/usr/src/${OFFER_FILE}`;
+const FROM_ADDRESS = 'agoric1ee9hr0jyrxhy999y755mp862ljgycmwyp4pl7q';
+const { log, error } = console;
+
+try {
+  log('--- Creating NFA ---');
+
+  log('Preparing offer...');
+  const offer = await prepareOffer({
+    publicInvitationMaker: 'createNfa',
+    instanceName: 'createNfaV1',
+    brandName: 'BLD',
+    amount: 20_000_000n,
+    source: 'contract',
+  });
+
+  await processWalletOffer({
+    offer,
+    OFFER_FILE,
+    CONTAINER_PATH,
+    FROM_ADDRESS,
+  });
+} catch (err) {
+  error('ERROR:', err.shortMessage || err.message);
+  process.exit(1);
+}
