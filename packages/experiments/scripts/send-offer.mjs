1b27054dc784f5ae325778a3c83ca5227a6231f9
rabi-siddique <rabisiddique27@gmail.com>
2025-12-11 21:25:22 +0500

    changes to send-offer

--- packages/experiments/scripts/send-offer.mjs
+++ packages/experiments/scripts/send-offer.mjs
@@ -9,17 +9,14 @@ const FROM_ADDRESS = 'agoric1ee9hr0jyrxhy999y755mp862ljgycmwyp4pl7q';
 const { log, error } = console;
 
 try {
-  log('Preparing offer...');
+  log('Preparing offer to increment counter...');
   const offer = await prepareOffer({
-    publicInvitationMaker: 'vPusherInvitation',
-    instanceName: 'resolverMock',
-    brandName: 'BLD',
-    amount: 20_000_000n,
+    publicInvitationMaker: 'incrementInvitation',
+    instanceName: 'counter',
+    // brandName: 'BLD',
+    // amount: 20_000_000n,
     source: 'contract',
-    offerArgs: {
-      vPath: 'position1',
-      vData: { name: 'test' },
-    },
+    offerArgs: {},
   });
 
   await processWalletOffer({
@@ -29,6 +26,6 @@ try {
     FROM_ADDRESS,
   });
 } catch (err) {
-  error('ERROR:', err.shortMessage || err.message);
+  error('ERROR:', err);
   process.exit(1);
 }
